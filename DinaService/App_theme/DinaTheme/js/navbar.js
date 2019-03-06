5b744b6618d5803a12f414fa9bee56f48b83681c
mahdi hamidi <mahdihamidiamin@gmail.com>
2019-03-06 16:21:40 +0330

    13971215

--- DinaService/App_theme/DinaTheme/js/navbar.js
+++ DinaService/App_theme/DinaTheme/js/navbar.js
@@ -2,26 +2,32 @@
 
     navbar();
     function navbar() {
-
         $('.list-nav > li  ul').addClass('dropdown-container');
-
-
-
-      
     }
 
-    $(".list-nav > li ").click(function () {
 
-        if ($('.icon-nav', this).hasClass("icon-nav-change")) {
-            $('.icon-nav', this).addClass('').removeClass('icon-nav-change');
+    $(".icon-nav").click(function () {
+
+        if ($(this).hasClass("icon-nav-change")) {
+            $('.icon-nav').addClass('').removeClass('icon-nav-change');
+            $('.sidenav').removeClass('open-nav').addClass('close-nav');
         }
         else {
-            $('.icon-nav', this).addClass('icon-nav-change');
+            $('.icon-nav').addClass('icon-nav-change');
+            $('.sidenav').removeClass('close-nav').addClass('open-nav');
         }
     });
 
+    $(".list-nav > li ").click(function () {
 
-
+        if ($('.btn-menu', this).hasClass("icon-nav-change")) {
+            $('.btn-menu', this).addClass('').removeClass('icon-nav-change');
+        }
+        else {
+            $('.list-nav > li .btn-menu').addClass('').removeClass('icon-nav-change');
+            $('.btn-menu', this).addClass('icon-nav-change');
+        }
+    });
 
     $('.mask').on('click', function () {
         $('.icon-nav').addClass('').removeClass('icon-nav-change');
@@ -33,30 +39,48 @@
 
     $("#accordian li")
         .click(function () {
+            if ($(this).hasClass('active')) {
+                $(this).removeClass('active');
+            }
             $(this).addClass('active');
         });
     $("#accordian a")
         .click(function () {
-
             var link = $(this);
             var closestUl = link.closest("ul");
             var parallelActiveLinks = closestUl.find(".active");
             var closestLi = link.closest("li");
             var linkStatus = closestLi.hasClass("active");
             var count = 0;
+            if ($(window).width() >= 750) {
 
-            closestUl.find("ul")
-                .slideUp(function () {
-                    if (++count == closestUl.find("ul").length)
-                        parallelActiveLinks.removeClass("active");
-                });
 
-            if (!linkStatus) {
-                closestLi.children("ul").slideDown();
-                closestLi.addClass("active");
+                closestUl.find("ul")
+                    .fadeOut(function () {
+                        if (++count == closestUl.find("ul").length)
+                            parallelActiveLinks.removeClass("active");
+                    });
+
+                if (!linkStatus) {
+                    closestLi.children("ul").fadeIn();
+                    closestLi.addClass("active");
+                }
+            } else {
+                closestUl.find("ul")
+                    .slideUp(function () {
+                        if (++count == closestUl.find("ul").length)
+                            parallelActiveLinks.removeClass("active");
+                    });
+
+                if (!linkStatus) {
+                    closestLi.children("ul").slideDown();
+                    closestLi.addClass("active");
+                }
             }
         });
-
+    $('.search-box > a ').click(function() {
+        $('.search-element').addClass('open');
+    });
 
     $(document).scroll(function () {
         navbar();
