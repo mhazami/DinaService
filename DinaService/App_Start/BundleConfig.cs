1bea7cf1c596147df6e1f89017c716f8ba644514
Mohammad Azami <mohammadazami98@yahoo.com>
2019-03-21 20:49:13 +0330

    13980101

--- DinaService/App_Start/BundleConfig.cs
+++ DinaService/App_Start/BundleConfig.cs
@@ -6,22 +6,54 @@ namespace DinaService
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/App_theme/DinaTheme/js/jquery-{version}.js"));
+           
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/App_theme/DinaTheme/js/jquery.validate*"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //            "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            "~/App_theme/DinaTheme/js/modernizr-*"));
 
-          
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //            "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/App_theme/DinaTheme/js/bootstrap.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //          "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/all").Include(
+                      "~/App_theme/DinaTheme/js/all.js",
+                      "~/App_theme/DinaTheme/js/LoadPage.js"));
 
-            //bundles.Add(new StyleBundle("~/Content/css").Include(
-            //          "~/Content/bootstrap.css",
-            //          "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/body").Include(
+                      "~/App_theme/DinaTheme/js/navbar.js",
+                      "~/App_theme/DinaTheme/slider/assets/tether/tether.min.js",
+                      "~/App_theme/DinaTheme/slider/assets/bootstrap/js/bootstrap.min.js",
+                      "~/App_theme/DinaTheme/slider/assets/vimeoplayer/jquery.mb.vimeo_player.js",
+                      "~/App_theme/DinaTheme/slider/assets/theme/js/script.js",
+                      "~/App_theme/DinaTheme/slider/assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js",
+                      "~/App_theme/DinaTheme/slider/assets/smoothscroll/smooth-scroll.js",
+                      "~/App_theme/DinaTheme/slider/assets/slidervideo/script.js",
+                      "~/App_theme/DinaTheme/js/owl.carousel.min.js"));
+            //style
+            bundles.Add(new StyleBundle("~/bootstrap/css").Include(
+                      "~/App_theme/DinaTheme/css/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/site/css").Include(
+                     "~/App_theme/DinaTheme/css/all.css",
+                     "~/App_theme/DinaTheme/slider/assets/web/assets/mobirise-icons/mobirise-icons.css",
+                     "~/App_theme/DinaTheme/slider/assets/bootstrap/css/bootstrap.min.css",
+                     "~/App_theme/DinaTheme/slider/assets/mobirise/css/mbr-additional.css",
+                     "~/App_theme/DinaTheme/css/NavMenu.css",
+                     "~/App_theme/DinaTheme/css/nav-style.css",
+                     "~/App_theme/DinaTheme/css/owl.carousel.min.css",
+                     "~/App_theme/DinaTheme/css/owl.theme.default.min.css",
+                     "~/App_theme/DinaTheme/css/aos.css"));
+
+            bundles.Add(new StyleBundle("~/slider/css").Include(
+                      "~/App_theme/DinaTheme/css/r-slider.css"));
+
+            bundles.Add(new StyleBundle("~/style/css").Include(
+                    "~/App_theme/DinaTheme/css/style.css"));
+
+
         }
     }
 }
---
+++ DinaService/App_theme/DinaTheme/js/LoadPage.js
@@ -0,0 +1,14 @@
+﻿var myVar;
+var myVar1;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 2000);
+    myVar = setTimeout(aos, 3000);
+
+}
+
+function showPage() {
+    document.getElementById("atomo").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+
+}
